8f23cd4ea6856350176a74427b9436a83fcd861e
SiddharthaSauravGogoi <lezaroth3@gmail.com>
2020-08-13 22:44:53 +0530

    Updates

---
+++ precache-manifest.33c3074ee49800f8308b503f355034f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f43fa20296a8dd3a47241e134496f9",
+    "url": "/spacex-launch-dash/index.html"
+  },
+  {
+    "revision": "01d858aeb0f0c9023bee",
+    "url": "/spacex-launch-dash/static/css/2.81de0c22.chunk.css"
+  },
+  {
+    "revision": "c4bede48c22212da9dbb",
+    "url": "/spacex-launch-dash/static/css/main.7b822dab.chunk.css"
+  },
+  {
+    "revision": "01d858aeb0f0c9023bee",
+    "url": "/spacex-launch-dash/static/js/2.93a67c98.chunk.js"
+  },
+  {
+    "revision": "0808c75b50af43a39fe893124e2f5e72",
+    "url": "/spacex-launch-dash/static/js/2.93a67c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bede48c22212da9dbb",
+    "url": "/spacex-launch-dash/static/js/main.cc2745fd.chunk.js"
+  },
+  {
+    "revision": "e1c82a1c8ad24c36e389",
+    "url": "/spacex-launch-dash/static/js/runtime-main.fe154cdc.js"
+  },
+  {
+    "revision": "ba569accb1a69c9753b2f67d4dab164f",
+    "url": "/spacex-launch-dash/static/media/spinner.ba569acc.gif"
+  }
+]);
